55e39eaed9f04d4b831abefd22442f2b0399692e
Josevi97 <josevi00@hotmail.com>
2021-12-19 15:09:22 +0100

    spinner added

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,10 +6,21 @@ let idSession, idUser, idDocumento;
 
 document.addEventListener('submit', (e) => {
 	e.preventDefault();
+
+	showSpinner();
+
 	correo = document.getElementById('correo').value;
 	password = document.getElementById('password').value;
 
-	createSession(correo, password, () => {
-		init();
-	});
+	createSession(
+		correo,
+		password,
+		() => {
+			init();
+		},
+		() => {
+			console.log('Deberia mostrar una alerta de error');
+			hideSpinner();
+		}
+	);
 });
